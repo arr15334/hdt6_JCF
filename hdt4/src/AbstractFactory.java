0f25e6b480e4b05acf10f2a2e41fe2979565ab7d
arriaza96 <arriaza96@Rodrigo-pc>
2016-08-17 20:42:02 -0600

    v7 correccion de errores

--- hdt4/src/AbstractFactory.java
+++ hdt4/src/AbstractFactory.java
@@ -3,6 +3,13 @@
  * @author Rodrigo Arriaza;
  */
 public abstract class AbstractFactory {
+
+    /**
+     *
+     * @param <E>
+     * @param tipoStack
+     * @return
+     */
     public abstract <E> Stack<E> makeStack(String tipoStack);
 }
 
