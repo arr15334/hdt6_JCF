1fc4a1c6abeff45ef87dbc5bd4a0edbd7f514990
arriaza96 <arriaza96@172.20.4.66>
2016-08-31 11:11:02 -0600

    v0

--- hdt6/src/Factory.java
+++ hdt6/src/Factory.java
@@ -4,12 +4,6 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Rodrigo Arriaza;
